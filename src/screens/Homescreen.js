a3ab78a9df6b021eb9ed8db569cc7ef5954c3c6d
krishnavamsipotla <potlakrishna13@gmail.com>
2023-11-23 21:29:12 +0530

    final

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,6 +1,88 @@
 
+// import React, { useState } from 'react';
+// import { View, TextInput, Button, StyleSheet ,Image,ImageBackground} from 'react-native';
+// import { useDispatch } from 'react-redux';
+// import { setFullName } from '../actions/quizActions';
+
+// const HomeScreen = ({ navigation }) => {
+//   const dispatch = useDispatch();
+//   const [fullName, setFullNameLocal] = useState('');
+
+//   const handlePressSubmit = () => {
+//     dispatch(setFullName(fullName));
+//     navigation.navigate('QuizForm');
+//   };
+
+//   return (
+//     <ImageBackground
+//     source={require('../images/image6.jpg')} // Replace with the actual path to your image
+//     style={styles.background}
+//   >
+//     <View style={styles.container}>
+//               <Image
+//           source={require('../images/image3.webp')}
+//           style={styles.logo}
+//         />
+
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Enter Full Name"
+//         value={fullName}
+//         onChangeText={setFullNameLocal}
+//         keyboardAppearance="light" // Set to 'light' for a light-themed keyboard
+
+//       />
+//       <Button title="START QUIZ"  onPress={handlePressSubmit} color={'orange'} />
+//     </View>
+//      </ImageBackground>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   background: {
+//     flex: 1,
+//     resizeMode: 'center',
+//     justifyContent: 'center',
+//     opacity:0.8,
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     // backgroundColor:'goldenrod',
+//     opacity:1
+
+//   },
+//   logo: {
+//     width: 250,
+//     height: 250,
+//     borderWidth:40,
+//     marginBottom: 40,
+//     opacity:1
+//     // Additional styling for the logo if needed
+//   },
+//   input: {
+//     height: 50,
+//     width: '80%',
+//     borderColor: 'orange',
+//     borderWidth: 2,
+//     // marginTop:40,
+//     marginBottom: 40,
+//     padding: 10,
+//     borderRadius:15,
+//     fontSize:20,
+//     fontWeight:'bold',
+//     color:'black'
+//   },
+
+
+// });
+
+// export default HomeScreen;
+
+
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet ,Image,ImageBackground} from 'react-native';
+import { View, TextInput, Button, StyleSheet, Image, ImageBackground, Alert } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { setFullName } from '../actions/quizActions';
 
@@ -9,32 +91,36 @@ const HomeScreen = ({ navigation }) => {
   const [fullName, setFullNameLocal] = useState('');
 
   const handlePressSubmit = () => {
+    
+    if (!fullName.trim()) {
+      Alert.alert('Alert', 'Please enter your  Name.');
+      return;
+    }
     dispatch(setFullName(fullName));
     navigation.navigate('QuizForm');
   };
 
   return (
     <ImageBackground
-    source={require('../images/image6.jpg')} // Replace with the actual path to your image
-    style={styles.background}
-  >
-    <View style={styles.container}>
-              <Image
+      source={require('../images/image6.jpg')} 
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Image
           source={require('../images/image3.webp')}
           style={styles.logo}
         />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Enter Full Name"
-        value={fullName}
-        onChangeText={setFullNameLocal}
-        keyboardAppearance="light" // Set to 'light' for a light-themed keyboard
-
-      />
-      <Button title="START QUIZ"  onPress={handlePressSubmit} color={'orange'} />
-    </View>
-     </ImageBackground>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Full Name"
+          value={fullName}
+          onChangeText={setFullNameLocal}
+          keyboardAppearance="light" 
+        />
+        <Button title="START QUIZ" onPress={handlePressSubmit} color={'orange'} />
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -43,39 +129,34 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: 'center',
     justifyContent: 'center',
-    opacity:0.8,
+    opacity: 0.8,
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor:'goldenrod',
-    opacity:1
-
+    opacity: 1,
   },
   logo: {
     width: 250,
     height: 250,
-    borderWidth:40,
+    borderWidth: 40,
     marginBottom: 40,
-    opacity:1
-    // Additional styling for the logo if needed
+    opacity: 1,
   },
   input: {
     height: 50,
     width: '80%',
     borderColor: 'orange',
     borderWidth: 2,
-    // marginTop:40,
     marginBottom: 40,
     padding: 10,
-    borderRadius:15,
-    fontSize:20,
-    fontWeight:'bold',
-    color:'black'
+    borderRadius: 15,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
   },
-
-
 });
 
 export default HomeScreen;
+
